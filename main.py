54aabc2a26f16ec3cb4eea7291abca151d2a4441
hayato423 <49159244+hayato423@users.noreply.github.com>
2020-11-11 22:15:00 +0900

    BPM,pNN表示

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import serial
 from serial.tools import list_ports
+import numpy as np
+from datetime import datetime
+from time import sleep
 
 def start_serial():
     ser = serial.Serial()
@@ -31,13 +34,67 @@ def start_serial():
         return None
 
 
-ser = start_serial()
-while ser.is_open:
-    bin_data = ser.read(256)
+def get_data(ser):
+    bin_data = ser.readline()
     if bin_data != b'':
         data = bin_data.decode()
         data = data.strip()
-        print(data)
+        return data
+    else:
+        return None
+
+def pNN():
+    print('pnn')
+
+
+
+ser = start_serial()
+bpm = 0
+ibi = 0
+bpm_prev = 0
+ibi_prev = 0
+pNN_data = np.zeros(30)
+pNN_count = 0
+checker = 1
+datalist = 0
+first_pNN = 0
+
+print("start")
+print('please wait')
+while ser.is_open:
+    data = get_data(ser)
+    if data != None:
+        if data[0] == "B":
+            bpm = int(data[1:])
+        elif data[0] == "Q":
+            ibi = int(data[1:])
+    if bpm != 0 and ibi != 0:
+        if bpm == bpm_prev and ibi == ibi_prev:
+            checker = 1
+        if checker != 1:
+            print(datetime.now().strftime('%H:%M:%S'))
+            if ibi_prev != 0 and bpm_prev != 0:
+                pNN_n = ibi - ibi_prev
+                if pNN_n*pNN_n >= 2500:
+                    pNN_data[pNN_count] = 1
+                else:
+                    pNN_data[pNN_count] = 0
+                pNN_count += 1
+                if pNN_count == 30:
+                    datalist = 1
+                    pNN_count = 0
+
+                if datalist == 1:
+                    pNN_sum = np.sum(pNN_data)
+                    pNN_ave = pNN_sum / 30
+                    if first_pNN == 0:
+                        first_pNN = pNN_ave
+                        print("\nfirst pNN is {}".format(first_pNN))
+                    print("BPM:{}  pNN:{}".format(bpm,pNN_ave))
+            ibi_prev = ibi
+            bpm_prev = bpm
+        checker = 0
+
 ser.close()
 
 
